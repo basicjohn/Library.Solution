e827191603a29ff10b9c682dca16aa5a3aea55a6
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-01 10:43:19 -0700

    add some boiler plate, begin building models
    Co-authored-by: John Edmondson <edmondsonj@gmail.com>

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+  public class Book
+  {
+    public Book()
+    {
+
+
+    }
+  }
+}
