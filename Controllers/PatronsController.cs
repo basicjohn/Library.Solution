7098115c27212d76f61648af205c44d35ed6d69f
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-01 14:06:54 -0700

    add controllers homie
    Co-authored-by: John Edmondson <edmondsonj@gmail.com>

---
+++ Controllers/PatronsController.cs
@@ -0,0 +1,126 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Library.Controllers
+{
+  [Authorize]
+  public class PatronsController : Controller
+  {
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public PatronsController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userPatrons = _db.Patrons.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userPatrons);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.BookId = new SelectList(_db.Books, "BookId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Patron patron, int BookId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      patron.User = currentUser;
+      _db.Patrons.Add(patron);
+      _db.SaveChanges();
+      if (BookId != 0)
+      {
+        _db.Checkout.Add(new Checkout() { BookId = BookId, PatronId = patron.PatronId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisPatron = _db.Patrons
+          .Include(patron => patron.JoinBookPatron)
+          .ThenInclude(join => join.Book)
+          .FirstOrDefault(patron => patron.PatronId == id);
+      return View(thisPatron);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      ViewBag.BookId = new SelectList(_db.Books, "BookId", "Name");
+      return View(thisPatron);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Patron patron, int BookId)
+    {
+      if (BookId != 0)
+      {
+        _db.Checkout.Add(new Checkout() { BookId = BookId, PatronId = patron.PatronId });
+      }
+      _db.Entry(patron).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddBook(int id)
+    {
+      var thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      ViewBag.BookId = new SelectList(_db.Books, "BookId", "Name");
+      return View(thisPatron);
+    }
+
+    [HttpPost]
+    public ActionResult AddBook(Patron patron, int BookId)
+    {
+      if (BookId != 0)
+      {
+        _db.Checkout.Add(new Checkout() { BookId = BookId, PatronId = patron.PatronId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      return View(thisPatron);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      _db.Patrons.Remove(thisPatron);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteBook(int joinId)
+    {
+      var joinEntry = _db.Checkout.FirstOrDefault(entry => entry.CheckoutId == joinId);
+      _db.Checkout.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
